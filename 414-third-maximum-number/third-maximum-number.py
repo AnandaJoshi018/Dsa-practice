45ce066a80817d7a8e9faa1046d83389378c85cd
Ananda Joshi <anandjoshi050@gmail.com>
2026-08-23 19:53:57 +0530

    Time: 1 ms (42.74%) | Memory: 20.6 MB (7.98%) - LeetSync

--- 414-third-maximum-number/third-maximum-number.py
+++ 414-third-maximum-number/third-maximum-number.py
@@ -1,7 +1,7 @@
 class Solution:
     def thirdMax(self, nums: List[int]) -> int:
-        nums = list(set(nums))
-        nums.sort()
+        nums = list(set(nums)) # this removes duplicate elemensts in list/array
+        nums.sort() # sorts in ascending order
 
         if len(nums) < 3 :
             return nums[-1]
